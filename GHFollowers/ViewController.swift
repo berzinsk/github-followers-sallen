6f9c00af7edb1fe7d8d06c3e64818d8593ef7a8d
Karlis Berzins <berzinsh.karlis@gmail.com>
2023-02-10 13:59:07 +0200

    Initial commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Karlis Berzins on 10/02/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
