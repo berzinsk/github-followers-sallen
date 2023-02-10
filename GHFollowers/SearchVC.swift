53c71e815c1e4ebf0693729336614527eaca07bf
Karlis Berzins <berzinsh.karlis@gmail.com>
2023-02-10 19:14:46 +0200

    Update indentation / Remove whitespaces

--- GHFollowers/SearchVC.swift
+++ GHFollowers/SearchVC.swift
@@ -8,10 +8,9 @@
 import UIKit
 
 class SearchVC: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
+    override func viewDidLoad() {
+        super.viewDidLoad()
     
-    view.backgroundColor = .systemPink
-  }
+        view.backgroundColor = .systemPink
+    }
 }
