9eb3bd3c11f82c2c78f35b960f4184717557d880
Karlis Berzins <berzinsh.karlis@gmail.com>
2023-02-11 14:48:10 +0200

    Create Follower cell

---
+++ GHFollowers/CustomViews/GFAvatarImageView.swift
@@ -0,0 +1,33 @@
+//
+//  GFAvatarImageView.swift
+//  GHFollowers
+//
+//  Created by Karlis Berzins on 11/02/2023.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+    private let placeholderImage = UIImage(named: "avatar-placeholder")
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        configure()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    init() {
+        super.init(frame: .zero)
+    }
+
+    private func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
