becd5506ef8b58c988cd7fcaba2ba1c3bac80af8
Karlis Berzins <berzinsh.karlis@gmail.com>
2023-02-11 10:46:48 +0200

    Create models for APIs

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Karlis Berzins on 11/02/2023.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
