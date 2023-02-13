6a532d2cd835bb0d1c671bc1e6c6278009193329
Karlis Berzins <berzinsh.karlis@gmail.com>
2023-02-13 06:57:50 +0200

    Add get user info API call

--- GHFollowers/Screens/UserInfoVC.swift
+++ GHFollowers/Screens/UserInfoVC.swift
@@ -16,6 +16,15 @@ class UserInfoVC: UIViewController {
         view.backgroundColor = .systemBackground
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
         navigationItem.rightBarButtonItem = doneButton
+
+        NetworkProvider.shared.getUserInfo(for: username) { [weak self] result in
+            switch result {
+            case .success(let user):
+                print(user)
+            case .failure(let error):
+                self?.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
+            }
+        }
     }
 
     @objc private func dismissVC() {
