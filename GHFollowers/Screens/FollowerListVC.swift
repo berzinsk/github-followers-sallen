ab06a194232653b866a425bab8aa9a1cdadfc5a9
Karlis Berzins <berzinsh.karlis@gmail.com>
2023-02-14 14:13:17 +0200

    Refactor GFDataLoadingVC, empty state view and follower list bug fixes

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -26,6 +26,7 @@ class FollowerListVC: GFDataLoadingVC {
     private var page = 1
     private var hasMoreFollowers = true
     private var isSearching = false
+    private var isLoadingMoreFollowers = false
 
     init(username: String) {
         super.init(nibName: nil, bundle: nil)
@@ -79,6 +80,7 @@ class FollowerListVC: GFDataLoadingVC {
 
     private func getFollowers(username: String, page: Int) {
         showLoadingView()
+        isLoadingMoreFollowers = true
 
         NetworkProvider.shared.getFollowers(for: username, page: page) { [weak self] result in
             guard let self else { return }
@@ -105,6 +107,8 @@ class FollowerListVC: GFDataLoadingVC {
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "Ok")
             }
+
+            self.isLoadingMoreFollowers = false
         }
     }
 
@@ -162,7 +166,7 @@ extension FollowerListVC: UICollectionViewDelegate {
         let height = scrollView.frame.size.height
 
         if offsetY > contentHeight - height {
-            guard hasMoreFollowers else { return }
+            guard hasMoreFollowers, !isLoadingMoreFollowers else { return }
             page += 1
             getFollowers(username: username, page: page)
         }
@@ -202,7 +206,7 @@ extension FollowerListVC: FollowerListVCDelegate {
         page = 1
         followers.removeAll()
         filteredFollowers.removeAll()
-        collectionView.setContentOffset(.zero, animated: true)
+        collectionView.scrollToItem(at: IndexPath(item: 0, section: 0), at: .top, animated: true)
         getFollowers(username: username, page: page)
     }
 }
