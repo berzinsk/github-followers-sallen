82d097142bb416b7f760a70e8a4ab592be83d400
Karlis Berzins <berzinsh.karlis@gmail.com>
2023-02-14 07:14:18 +0200

    Add table view to favorites list tab

--- GHFollowers/Screens/FavoritesListVC.swift
+++ GHFollowers/Screens/FavoritesListVC.swift
@@ -8,18 +8,61 @@
 import UIKit
 
 class FavoritesListVC: UIViewController {
+    private let tableView = UITableView()
+    private var favorites: [Follower] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .systemBlue
+        configureViewController()
+        configureTableView()
+        getFavorites()
+    }
 
-        PersistenceProvider.retrieveFavorites { result in
+    private func configureViewController() {
+        view.backgroundColor = .systemBackground
+        title = "Favorites"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+    private func configureTableView() {
+        view.addSubview(tableView)
+        tableView.frame = view.bounds
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(FavoriteCell.self, forCellReuseIdentifier: FavoriteCell.reuseId)
+    }
+
+    private func getFavorites() {
+        PersistenceProvider.retrieveFavorites { [weak self] result in
+            guard let self else { return }
+
             switch result {
             case .success(let favorites):
-                print(favorites)
+                self.favorites = favorites
             case .failure(let error):
                 print(error.rawValue)
             }
         }
+    }
+}
+
+extension FavoritesListVC: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favorites.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: FavoriteCell.reuseId, for: indexPath) as! FavoriteCell
+        let favorite = favorites[indexPath.row]
+
+        cell.set(favorite: favorite)
+
+        return cell
     }
 }
