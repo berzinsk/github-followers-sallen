0a62ee196e2dee8a58099d9c42d0d9b433da983f
Karlis Berzins <berzinsh.karlis@gmail.com>
2023-02-14 07:19:28 +0200

    Add option to open favorite details and remove favorite

--- GHFollowers/Screens/FavoritesListVC.swift
+++ GHFollowers/Screens/FavoritesListVC.swift
@@ -16,6 +16,11 @@ class FavoritesListVC: UIViewController {
 
         configureViewController()
         configureTableView()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
         getFavorites()
     }
 
@@ -30,7 +35,7 @@ class FavoritesListVC: UIViewController {
         tableView.frame = view.bounds
         tableView.delegate = self
         tableView.dataSource = self
-        
+
         tableView.register(FavoriteCell.self, forCellReuseIdentifier: FavoriteCell.reuseId)
     }
 
@@ -40,9 +45,17 @@ class FavoritesListVC: UIViewController {
 
             switch result {
             case .success(let favorites):
-                self.favorites = favorites
+                if favorites.isEmpty {
+                    self.showEmptyStateView(with: "No Favorites?\nAdd one on the follower screen.", in: self.view)
+                } else {
+                    self.favorites = favorites
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                        self.view.bringSubviewToFront(self.tableView)
+                    }
+                }
             case .failure(let error):
-                print(error.rawValue)
+                self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
             }
         }
     }
@@ -64,5 +77,29 @@ extension FavoritesListVC: UITableViewDataSource, UITableViewDelegate {
         cell.set(favorite: favorite)
 
         return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let favorite = favorites[indexPath.row]
+        let destVC = FollowerListVC()
+        destVC.username = favorite.login
+        destVC.title = favorite.login
+
+        navigationController?.pushViewController(destVC, animated: true)
+    }
+
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == .delete else { return }
+
+        let favorite = favorites[indexPath.row]
+        favorites.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .left)
+
+        PersistenceProvider.updateWith(favorite: favorite, actionType: .remove) { [weak self] error in
+            guard let self else { return }
+            guard let error else { return }
+
+            self.presentGFAlertOnMainThread(title: "Unable to remove", message: error.rawValue, buttonTitle: "Ok")
+        }
     }
 }
